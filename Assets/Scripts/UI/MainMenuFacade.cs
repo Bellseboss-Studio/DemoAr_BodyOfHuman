8f39d6c57b6cc81e80a6c61a1aad36b6575d9c29
Angel402 <bolo402402@gmail.com>
2023-03-21 21:54:57 -0600

    Main menu done

---
+++ Assets/Scripts/UI/MainMenuFacade.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenuFacade : MonoBehaviour
+    {
+        [SerializeField] private GameObject creditsPanel, mainPanel;
+        
+        public void StartApp()
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        public void ShowCredits()
+        {
+            creditsPanel.SetActive(true);
+            mainPanel.SetActive(false);
+        }
+        
+        public void HideCredits()
+        {
+            creditsPanel.SetActive(false);
+            mainPanel.SetActive(true);
+        }
+        
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
