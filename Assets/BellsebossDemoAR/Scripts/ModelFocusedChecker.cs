8a3e6fdc4121311b6241b8569138000a4821bffb
Angel402 <bolo402402@gmail.com>
2023-03-29 00:36:13 -0600

    sign and description for every organ done

---
+++ Assets/BellsebossDemoAR/Scripts/ModelFocusedChecker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BellsebossDemoAR.Scripts
+{
+    public class ModelFocusedChecker : MonoBehaviour
+    {
+        [SerializeField] private Camera ARCamera;
+        [SerializeField] private StateOfGame stateOfGame;
+        [SerializeField] private LayerMask layer;
+        private bool _objectWasInstantiated = false;
+        private ObjectInteractableInWord _objectInteractableInWord;
+        private bool _objectIsFocus = false;
+        private RaycastHit _rayCastHit;
+        private Transform _arCameraTransform;
+
+        private void Start()
+        {
+            _arCameraTransform = ARCamera.transform;
+            stateOfGame.OnInstantiateElement += () =>
+            {
+                _objectWasInstantiated = true;
+                stateOfGame.GetObjectInstantiate().TryGetComponent(out _objectInteractableInWord);
+                _objectInteractableInWord.OnChangeObject += () =>
+                {
+                    _objectIsFocus = false;
+                };
+            };
+        }
+
+        private void Update()
+        {
+            if (_objectWasInstantiated)
+            {
+                Debug.DrawRay(_arCameraTransform.position, _arCameraTransform.forward * 1000, Color.blue);
+                var rayCastInfo = Physics.RaycastAll(_arCameraTransform.position, _arCameraTransform.forward, 1000/*, layer.value*/);
+                if (rayCastInfo.Length >= 1)
+                {
+                    Debug.Log(rayCastInfo.Length);
+                    if (_objectIsFocus)
+                    {
+                        foreach (var raycastHit in rayCastInfo)
+                        {
+                            if (raycastHit.collider == _rayCastHit.collider)
+                            {
+                                return;
+                            }
+                        }
+                        CurrentOrganStoppedBeingFocused();
+                    }
+                    foreach (var raycastHit in rayCastInfo)
+                    {
+                        if (raycastHit.transform.gameObject == _objectInteractableInWord.GetCurrentOrgan())
+                        {
+                            CurrentOrganWasFocused(raycastHit);
+                        }
+                        else
+                        {
+                            if (raycastHit.transform.parent.gameObject == _objectInteractableInWord.GetCurrentOrgan())
+                            {
+                                CurrentOrganWasFocused(raycastHit);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log(rayCastInfo.Length);
+                    if (_objectIsFocus)
+                    {
+                        CurrentOrganStoppedBeingFocused();
+                    }
+                }
+            }
+        }
+
+        private void CurrentOrganStoppedBeingFocused()
+        {
+            _objectIsFocus = false;
+            _objectInteractableInWord.CurrentOrganStoppedBeingFocused();
+        }
+
+        private void CurrentOrganWasFocused(RaycastHit raycastHit)
+        {
+            _objectInteractableInWord.CurrentOrganWasFocused();
+            _objectIsFocus = true;
+            _rayCastHit = raycastHit;
+        }
+    }
+}
---
+++ Assets/BellsebossDemoAR/Scripts/Organ.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BellsebossDemoAR.Scripts
+{
+    public class Organ : MonoBehaviour
+    {
+        public List<Material> materials;
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private GameObject exclamationButton, description;
+
+        public void Configure()
+        {
+            gameObject.SetActive(true);
+            var renderers = gameObject.GetComponentsInChildren<Renderer>().ToList();
+            materials = new List<Material>();
+            foreach (var material in renderers.SelectMany(renderer1 => renderer1.materials))
+            {
+                materials.Add(material);
+            }
+        }
+
+        public void ShowOrganCanvas()
+        {
+            exclamationButton.SetActive(true);
+            description.SetActive(false);
+            canvas.gameObject.SetActive(true);
+        }
+
+        public void HideOrganCanvas()
+        {
+            canvas.gameObject.SetActive(false);
+        }
+
+        public void ShowDescription()
+        {
+            exclamationButton.SetActive(false);
+            description.SetActive(true);
+        }
+    }
+}
